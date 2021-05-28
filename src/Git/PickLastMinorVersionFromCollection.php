c1d108707d3397c8b809ad95a9d28d9dbd2198df
Ondrej Mirtes <ondrej@mirtes.cz>
2021-05-28 16:28:35 +0200

    Always check against the latest release
    See https://github.com/Roave/BackwardCompatibilityCheck/issues/307

--- src/Git/PickLastMinorVersionFromCollection.php
+++ src/Git/PickLastMinorVersionFromCollection.php
@@ -5,9 +5,7 @@ declare(strict_types=1);
 namespace Roave\BackwardCompatibility\Git;
 
 use Psl;
-use Version\Comparison\Constraint\CompositeConstraint;
 use Version\Comparison\Constraint\Constraint;
-use Version\Comparison\Constraint\OperationConstraint;
 use Version\Version;
 use Version\VersionCollection;
 
@@ -26,19 +24,6 @@ final class PickLastMinorVersionFromCollection implements PickVersionFromVersion
 
         $versionsSortedDescending = $stableVersions->sortedDescending();
 
-        $lastVersion = $versionsSortedDescending->first();
-
-        if ($lastVersion->getMajor() === 0) {
-            return $lastVersion;
-        }
-
-        $matchingMinorVersions = $stableVersions
-            ->matching(CompositeConstraint::and(
-                OperationConstraint::lessOrEqualTo($lastVersion),
-                OperationConstraint::greaterOrEqualTo(Version::fromString($lastVersion->getMajor() . '.' . $lastVersion->getMinor() . '.0'))
-            ))
-            ->sortedAscending();
-
-        return $matchingMinorVersions->first();
+        return $versionsSortedDescending->first();
     }
 }
